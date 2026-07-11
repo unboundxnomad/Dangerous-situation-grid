6a824312b1e106f58ebd6ac907d17a9ae544c613
unboundxnomad <unboundxnomad@gmail.com>
2026-07-11 17:00:46 +0530

    Day 2: Implemented hybrid matrix slicing and polygonal ROI mask

--- grid_core_vr2.py
+++ grid_core_vr2.py
@@ -1,5 +1,7 @@
 # pyrefly: ignore [missing-import]
 import cv2 
+# pyrefly: ignore [missing-import]
+import numpy as np
 
 # capturing the video 
 video_path = "/Users/purushothamanvenkatachalam/Downloads/Ml_project/Karwan Bazar Pedestrians.mp4"
@@ -16,14 +18,36 @@ while cap.isOpened():
         print("stream has ended ")
         break 
     # cropping the top portion of the video fo unnessary detailes like sky, birds etc won't be detected 
-    height , width ,_=frame.shape
+    height , width =frame.shape[:2]
 
-    #how much to cut 
+    #how much to cut from the top 
     roi_start_y = int(height*.25)
     cropped_frame=frame[roi_start_y:height,0:width]
 
+    #getting the height and width of the cropped video 
+    crop_h,crop_w=cropped_frame.shape[:2]
+
+    # applying polygon mask 
+    polygon_points=np.array([
+        [int(crop_w*0.1),crop_h],
+        [int(crop_w*.4),int(crop_h*.5)],
+        [int(crop_w*.6),int(crop_h*.5)],
+        [int(crop_w*.9),crop_h]
+    ],np.int32)
+    #reshape points for fillPoly
+    polygon_points=polygon_points.reshape((-1,1,2))
+
+    # creating a black mask exact size as the frame 
+    mask= np.zeros((crop_h,crop_w),dtype=np.uint8)
+
+    # filling the polygon area on the mask with white 
+    cv2.fillPoly(mask,[polygon_points],255)
+    
+    # applying this to the cropped frame 
+    poly_frame=cv2.bitwise_and(cropped_frame,cropped_frame,mask=mask)
+
     # apply grayScale  to reduce 3x of computations (3d to 2d)
-    gray_scale=cv2.cvtColor(cropped_frame,cv2.COLOR_BGR2GRAY)
+    gray_scale=cv2.cvtColor(poly_frame,cv2.COLOR_BGR2GRAY)
     # apply gausian blur
     blured=cv2.GaussianBlur(gray_scale,(5,5),0)
     # apply canny edge degtection
